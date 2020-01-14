247c8e13b147286fe416de4127b71a3c561eacb0
sabinbaniya <59755055+sabinbaniya@users.noreply.github.com>
2020-01-14 17:17:11 +0545

    Add files via upload

---
+++ New folder/divisible-by-6-and-7/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int number;
+    printf("Enter a number: ");
+    scanf("%d",&number);
+    if(number%6==0&&number%7==0){
+        printf("%d is divisible by both 6 & 7\n",number);
+    }else{
+        printf("%d is not divisible by both 6 & 7\n",number);
+    }
+    return 0;
+}
